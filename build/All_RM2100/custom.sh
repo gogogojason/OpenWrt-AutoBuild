d7fe9e0397949ec2fc4259f0a5e57b706aa4b6c1
icons <65128346+gogogojason@users.noreply.github.com>
2020-12-06 17:31:40 +0800

    Update custom.sh

--- build/All_RM2100/custom.sh
+++ build/All_RM2100/custom.sh
@@ -10,7 +10,7 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 git clone https://github.com/gogogojason/luci-theme-edge -b 18.06 package/lean/luci-theme-edge
-git clone https://github.com/db-one/dbone-update.git -b 18.06 package/dbone-update
+#git clone https://github.com/db-one/dbone-update.git -b 18.06 package/dbone-update
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/kenzok8/openwrt-packages.git package/otherpackages
 git clone https://github.com/281677160/openwrt-package.git package/otherpackages2
