90c6f98698a5cea6f7cc268dec1ae8f6bcf16f31
icons <65128346+gogogojason@users.noreply.github.com>
2020-12-12 06:16:16 +0800

    Update custom.sh

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -46,8 +46,8 @@ default_theme='luci-theme-edge'                                             # �
 openClash_url='https://github.com/vernesong/OpenClash.git'                  # OpenClash包地址
 #upgrade_url='https://github.com/gogogojason/upgrade.git'
 
-#echo "修改版本信息"
-sed -i "s/OpenWrt /hfy166 Ver.D$(TZ=UTC-8 date "+%Y.%m.%d") \/ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+echo "修改版本信息"
+sed -i "s/OpenWrt /hfy166 Ver.D$(TZ=UTC-8 date "+%Y.%m.%d") \/OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #echo "修改wifi名称"
 #sed -i "s/OpenWrt_2G/$wifi_name/g" package/lean/mt/drivers/mt_wifi/files/mt7603.dat
