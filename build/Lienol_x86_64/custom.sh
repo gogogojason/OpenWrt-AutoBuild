80ad1385d0983f65431b9c9f327f4dd685b02026
icons <65128346+gogogojason@users.noreply.github.com>
2020-12-16 19:02:56 +0800

    Update custom.sh

--- build/Lienol_x86_64/custom.sh
+++ build/Lienol_x86_64/custom.sh
@@ -44,7 +44,7 @@ openClash_url='https://github.com/vernesong/OpenClash.git'                  # Op
 #upgrade_url='https://github.com/gogogojason/upgrade.git'
 
 echo "修改版本信息"
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by hfy166'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/'%D %V %C'/DISTRIB_DESCRIPTION='%D %V %C by hfy166'/g" package/base-files/files/etc/openwrt_release
 
 echo "默认IP设置"
 sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
@@ -52,13 +52,16 @@ sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 echo "修改机器名称"
 sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
 
+echo "修改时区"
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
 echo "修改默认主题"
 sed -i 's/+luci-theme-bootstrap/+luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 sed -i "s/bootstrap/argon/g" feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 切换内核为5.4
-sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.9/g' target/linux/x86/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 # 创建自定义配置文件 - Lienol_x86_64
