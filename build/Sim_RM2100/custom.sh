c3fdb4b4ec710268845792f36d2ad3dd0dc02fa8
icons <65128346+gogogojason@users.noreply.github.com>
2020-12-19 14:59:33 +0800

    Update custom.sh

--- build/Sim_RM2100/custom.sh
+++ build/Sim_RM2100/custom.sh
@@ -20,7 +20,7 @@ git clone https://github.com/Lienol/openwrt-package.git package/Lienol
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/kenzok8/openwrt-packages.git package/otherpackages
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-#git clone https://github.com/281677160/openwrt-package.git package/otherpackages2
+git clone https://github.com/281677160/openwrt-package.git package/otherpackages2
 
 
 # 更新并安装源
