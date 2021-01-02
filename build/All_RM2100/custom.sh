ad94045036e1c89041fa2a25d43ac1c87653332b
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-02 15:45:37 +0800

    Update custom.sh

--- build/All_RM2100/custom.sh
+++ build/All_RM2100/custom.sh
@@ -153,55 +153,65 @@ CONFIG_PACKAGE_luci-app-accesscontrol=y #上网时间控制
 EOF
 
 # VSSR插件:
-cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-vssr=y
-CONFIG_PACKAGE_luci-app-vssr_INCLUDE_V2ray_plugin=y
-CONFIG_PACKAGE_luci-app-vssr_INCLUDE_V2ray=y
-CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Xray=y
+#cat >> .config <<EOF
+#CONFIG_PACKAGE_luci-app-vssr=y
+#CONFIG_PACKAGE_luci-app-vssr_INCLUDE_V2ray_plugin=y
+#CONFIG_PACKAGE_luci-app-vssr_INCLUDE_V2ray=y
+#CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Xray=y
 # CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Trojan is not set
 # CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Kcptun is not set
-CONFIG_PACKAGE_luci-app-vssr_INCLUDE_ShadowsocksR_Server=y
+#CONFIG_PACKAGE_luci-app-vssr_INCLUDE_ShadowsocksR_Server=y
+#EOF
+
+# ShadowsocksR插件:
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-ssr-plus=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ShadowsocksR_Socks=y
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Kcptun is not set
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray=y
 EOF
 
 # Passwall插件:
 cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-passwall=y
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ipt2socks is not set
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ipt2socks=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y
 CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR=y
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG=y
 CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y
 CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=y
 CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=y
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=y
 # CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_GO is not set
 # CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook is not set
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun is not set
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_haproxy is not set
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_dns2socks is not set
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_pdnsd is not set
-# CONFIG_PACKAGE_https-dns-proxy is not set
-# CONFIG_PACKAGE_kcptun-client is not set
-# CONFIG_PACKAGE_chinadns-ng is not set
-# CONFIG_PACKAGE_haproxy is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_haproxy=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_dns2socks=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_pdnsd=y
+CONFIG_PACKAGE_https-dns-proxy=y
+CONFIG_PACKAGE_kcptun-client=y
+CONFIG_PACKAGE_chinadns-ng=y
+CONFIG_PACKAGE_haproxy=y
 CONFIG_PACKAGE_xray=y
 CONFIG_PACKAGE_v2ray=y
 CONFIG_PACKAGE_v2ray-plugin=y
 CONFIG_PACKAGE_simple-obfs=y
-# CONFIG_PACKAGE_trojan-plus is not set
-# CONFIG_PACKAGE_trojan-go is not set
-# CONFIG_PACKAGE_brook is not set
-# CONFIG_PACKAGE_ssocks is not set
-# CONFIG_PACKAGE_naiveproxy is not set
-# CONFIG_PACKAGE_ipt2socks is not set
-# CONFIG_PACKAGE_shadowsocks-libev-config is not set
-# CONFIG_PACKAGE_shadowsocks-libev-ss-local is not set
-# CONFIG_PACKAGE_shadowsocks-libev-ss-redir is not set
-# CONFIG_PACKAGE_shadowsocksr-libev-alt is not set
-# CONFIG_PACKAGE_shadowsocksr-libev-ssr-local is not set
-# CONFIG_PACKAGE_pdnsd-alt is not set
-# CONFIG_PACKAGE_dns2socks is not set
+CONFIG_PACKAGE_trojan-plus=y
+CONFIG_PACKAGE_trojan-go=y
+CONFIG_PACKAGE_brook=y
+CONFIG_PACKAGE_ssocks=y
+CONFIG_PACKAGE_naiveproxy=y
+CONFIG_PACKAGE_ipt2socks=y
+CONFIG_PACKAGE_shadowsocks-libev-config=y
+CONFIG_PACKAGE_shadowsocks-libev-ss-local=y
+CONFIG_PACKAGE_shadowsocks-libev-ss-redir=y
+CONFIG_PACKAGE_shadowsocksr-libev-alt=y
+CONFIG_PACKAGE_shadowsocksr-libev-ssr-local=y
+CONFIG_PACKAGE_pdnsd-alt=y
+CONFIG_PACKAGE_dns2socks=y
 EOF
+
 
 # vssr-plus插件配置#
 #cat >> .config <<EOF
