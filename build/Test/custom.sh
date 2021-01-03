a61341f4d3860366ef1b1aa1dc16f32e0ec77991
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-03 23:23:19 +0800

    Update custom.sh

--- build/Test/custom.sh
+++ build/Test/custom.sh
@@ -88,9 +88,9 @@ echo 'exit 0' >>package/lean/default-settings/files/zzz-default-settings
 #sed -i 's@e5effd@f8fbfe@g' package/dbone-update/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
 #sed -i 's#223, 56, 18, 0.04#223, 56, 18, 0.02#g' package/dbone-update/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
 
-#创建自定义配置文件 - Ap_RM2100
+#创建自定义配置文件 - Test
 
-cd build/Ap_RM2100
+cd build/Test
 touch ./.config
 
 # ========================固件定制部分========================
