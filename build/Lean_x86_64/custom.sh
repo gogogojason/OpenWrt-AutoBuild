89452abe21c44b7a03ffdd4be677e78d3752d1d6
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-14 22:59:33 +0800

    Update custom.sh

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-# 安装额外依赖软件包
-# sudo -E apt-get -y install rename
-
-# 更新feeds文件
-# sed -i 's@#src-git helloworld@src-git helloworld@g' feeds.conf.default #启用helloworld
-cat feeds.conf.default
-#sed -i '$a src-git otherpackages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
-#cat feeds.conf.default
-
-
 # 添加第三方软件包
 git clone https://github.com/gogogojason/OpenWrt-Packages.git package/jason
 git clone https://github.com/gogogojason/logos.git package/logos
-#git clone https://github.com/gogogojason/luci-theme-edge -b 18.06 package/lean/luci-theme-edge
-#git clone https://github.com/db-one/dbone-update.git -b 18.06 package/dbone-update
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/kenzok8/openwrt-packages.git package/otherpackages
-#git clone https://github.com/281677160/openwrt-package.git package/otherpackages2
-#git clone --depth=1 https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-#git clone https://github.com/Lienol/openwrt-package.git package/Lienol
 
 
 # 更新并安装源
@@ -34,33 +13,15 @@ git clone https://github.com/gogogojason/logos.git package/logos
 #rm -rf package/lean/luci-theme-argon
 #rm -rf feeds/packages/net/haproxy
 
-# 自定义定制选项
-# 定义部分以及需要添加对应APP必须的文件
-device_name='MyRouter'                                                      # 自定义设备名
-#wifi_name="RMWiFi"                                                          # 自定义Wifi 名字
-#wifi_name5g="RMWiFi_5G"                                                     # 自定义Wifi 名字
-lan_ip='192.168.2.1'                                                        # 自定义Lan Ip地址
-utc_name='Asia\/Shanghai'                                                   # 自定义时区
-date=`date +%m.%d.%Y`
-#ver_name='D201212'                                                          # 版本号
-#ver_op='R20.12.12'                                                          # 编译的版本
-delete_bootstrap=false                                                      # 是否删除默认主题 true 、false
-default_theme='luci-theme-edge'                                             # 默认主题 结合主题文件夹名字
-openClash_url='https://github.com/vernesong/OpenClash.git'                  # OpenClash包地址
-#upgrade_url='https://github.com/gogogojason/upgrade.git'
-
 echo "修改版本信息"
+date=`date +%m.%d.%Y`
 sed -i "s/OpenWrt /hfy166 Ver.D$(TZ=UTC-8 date "+%Y.%m.%d") \/ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-#echo "修改wifi名称"
-#sed -i "s/OpenWrt_2G/$wifi_name/g" package/lean/mt/drivers/mt_wifi/files/mt7603.dat
-#sed -i "s/OpenWrt_5G/$wifi_name5g/g" package/lean/mt/drivers/mt_wifi/files/mt7615.dat
-
 echo "默认IP设置"
-sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.2.1/g" package/base-files/files/bin/config_generate
 
 echo "修改机器名称"
-sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt/$MyRouter/g" package/base-files/files/bin/config_generate
 
 echo "修改ADG默认更新地址"
 sed -i 's/${Arch}_softfloat/${Arch}/g' package/jason/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
@@ -82,66 +43,18 @@ sed -i "91a\		$LOG notice \"Recycled $INTERFACE...\"" feeds/packages/net/mwan3/f
 echo "设置版本号"
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 
-
-#echo "时区设置"
-#sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
-
 echo "修改默认主题"
 sed -i 's/+luci-theme-bootstrap/+luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 sed -i "s/bootstrap/argon/g" feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-#echo "修改版本信息"
-#sed -i "s/$ver_op/$ver_op\/hfy166 Ver.$ver_name/g" package/lean/default-settings/files/zzz-default-settings
-
 #echo "取消默认密码"
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings #取消系统默认密码
-
-#echo "其他修改"
-#sed -i 's#option commit_interval 24h#option commit_interval 10m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计写入为10分钟
-#sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计数据存放默认位置
-#sed -i 's@interval: 5@interval: 1@g' package/lean/luci-app-wrtbwmon/htdocs/luci-static/wrtbwmon.js #wrtbwmon默认刷新时间更改为1秒
-#sed -i 's@%D %V, %C@%D %V, %C Lean_x86_64@g' package/base-files/files/etc/banner #自定义banner显示
-#sed -i 's@e5effd@f8fbfe@g' package/dbone-update/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
-#sed -i 's#223, 56, 18, 0.04#223, 56, 18, 0.02#g' package/dbone-update/luci-theme-edge/htdocs/luci-static/edge/cascade.css #luci-theme-edge主题颜色微调
 
 #创建自定义配置文件 - Lean_x86_64
 
 cd build/Lean_x86_64
 touch ./.config
-
-#
-# ========================固件定制部分========================
-# 
-
-# 
-# 如果不对本区块做出任何编辑, 则生成默认配置固件. 
-# 
-
-# 以下为定制化固件选项和说明:
-#
-
-#
-# 有些插件/选项是默认开启的, 如果想要关闭, 请参照以下示例进行编写:
-# 
-#          =========================================
-#         |  # 取消编译VMware镜像:                    |
-#         |  cat >> .config <<EOF                   |
-#         |  # CONFIG_VMDK_IMAGES is not set        |
-#         |  EOF                                    |
-#          =========================================
-#
-
-# 
-# 以下是一些提前准备好的一些插件选项.
-# 直接取消注释相应代码块即可应用. 不要取消注释代码块上的汉字说明.
-# 如果不需要代码块里的某一项配置, 只需要删除相应行.
-#
-# 如果需要其他插件, 请按照示例自行添加.
-# 注意, 只需添加依赖链顶端的包. 如果你需要插件 A, 同时 A 依赖 B, 即只需要添加 A.
-# 
-# 无论你想要对固件进行怎样的定制, 都需要且只需要修改 EOF 回环内的内容.
-# 
 
 # 编译Lean_x86_64固件:
 cat >> .config <<EOF
@@ -161,6 +74,94 @@ EOF
 cat >> .config <<EOF
 CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
 CONFIG_PACKAGE_ipv6helper=y
+EOF
+
+# Passwall插件:
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-passwall=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ipt2socks=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_GO=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_haproxy=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_dns2socks=y
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_pdnsd=y
+CONFIG_PACKAGE_https-dns-proxy=y
+CONFIG_PACKAGE_kcptun-client=y
+CONFIG_PACKAGE_chinadns-ng=y
+CONFIG_PACKAGE_haproxy=y
+CONFIG_PACKAGE_xray=y
+CONFIG_PACKAGE_v2ray=y
+CONFIG_PACKAGE_v2ray-plugin=y
+CONFIG_PACKAGE_simple-obfs=y
+CONFIG_PACKAGE_trojan-plus=y
+CONFIG_PACKAGE_trojan-go=y
+CONFIG_PACKAGE_brook=y
+CONFIG_PACKAGE_ssocks=y
+CONFIG_PACKAGE_naiveproxy=y
+CONFIG_PACKAGE_ipt2socks=y
+CONFIG_PACKAGE_shadowsocks-libev-config=y
+CONFIG_PACKAGE_shadowsocks-libev-ss-local=y
+CONFIG_PACKAGE_shadowsocks-libev-ss-redir=y
+CONFIG_PACKAGE_shadowsocksr-libev-alt=y
+CONFIG_PACKAGE_shadowsocksr-libev-ssr-local=y
+CONFIG_PACKAGE_pdnsd-alt=y
+CONFIG_PACKAGE_dns2socks=y
+EOF
+
+# 磁盘管理插件:
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-diskman=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_btrfs_progs=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_kmod_md_linear=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_kmod_md_raid456=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_mdadm=y
+EOF
+
+# 其他插件设置选择:
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-adguardhome=y
+CONFIG_PACKAGE_luci-app-gpsysupgrade=y
+CONFIG_PACKAGE_luci-app-frpc=y
+CONFIG_PACKAGE_luci-app-jd-dailybonus=y
+CONFIG_PACKAGE_luci-app-mwan3=y
+CONFIG_PACKAGE_luci-app-mwan3helper=y
+CONFIG_PACKAGE_luci-app-nps=y
+CONFIG_PACKAGE_luci-app-poweroff=y
+CONFIG_PACKAGE_luci-app-serverchan=y
+CONFIG_PACKAGE_luci-app-smartdns=y
+CONFIG_PACKAGE_luci-app-socat=y
+CONFIG_PACKAGE_luci-app-syncdial=y
+CONFIG_PACKAGE_luci-app-ttyd=y
+CONFIG_PACKAGE_luci-app-eqos=y
+#CONFIG_PACKAGE_luci-app-wrtbwmon-zh=y
+CONFIG_PACKAGE_smartdns=y
+EOF
+
+# 添加Hello World+:
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-bypass=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Shadowsocks_Server=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_ShadowsocksR_Server=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Simple_obfs=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Simple_obfs_server=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray_plugin=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Xray=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Trojan=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Trojan-Go=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_NaiveProxy=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Kcptun=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Socks5_Proxy=y
+CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Socks_Server=y
 EOF
 
 # 不安装插件:
@@ -193,45 +194,6 @@ cat >> .config <<EOF
 # CONFIG_UnblockNeteaseMusic_NodeJS is not set
 EOF
 
-# Passwall插件:
-#cat >> .config <<EOF
-#CONFIG_PACKAGE_luci-app-passwall=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ipt2socks=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_GO=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_haproxy=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_dns2socks=y
-#CONFIG_PACKAGE_luci-app-passwall_INCLUDE_pdnsd=y
-#CONFIG_PACKAGE_https-dns-proxy=y
-#CONFIG_PACKAGE_kcptun-client=y
-#CONFIG_PACKAGE_chinadns-ng=y
-#CONFIG_PACKAGE_haproxy=y
-#CONFIG_PACKAGE_xray=y
-#CONFIG_PACKAGE_v2ray=y
-#CONFIG_PACKAGE_v2ray-plugin=y
-#CONFIG_PACKAGE_simple-obfs=y
-#CONFIG_PACKAGE_trojan-plus=y
-#CONFIG_PACKAGE_trojan-go=y
-#CONFIG_PACKAGE_brook=y
-#CONFIG_PACKAGE_ssocks=y
-#CONFIG_PACKAGE_naiveproxy=y
-#CONFIG_PACKAGE_ipt2socks=y
-#CONFIG_PACKAGE_shadowsocks-libev-config=y
-#CONFIG_PACKAGE_shadowsocks-libev-ss-local=y
-#CONFIG_PACKAGE_shadowsocks-libev-ss-redir=y
-#CONFIG_PACKAGE_shadowsocksr-libev-alt=y
-#CONFIG_PACKAGE_shadowsocksr-libev-ssr-local=y
-#CONFIG_PACKAGE_pdnsd-alt=y
-#CONFIG_PACKAGE_dns2socks=y
-#EOF
 
 # VSSR专用插件:
 #cat >> .config <<EOF
@@ -244,55 +206,6 @@ EOF
 #CONFIG_PACKAGE_luci-app-vssr_INCLUDE_ShadowsocksR_Server=y
 #CONFIG_PACKAGE_lua-maxminddb=y
 #EOF
-
-# 磁盘管理插件:
-cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-diskman=y
-CONFIG_PACKAGE_luci-app-diskman_INCLUDE_btrfs_progs=y
-CONFIG_PACKAGE_luci-app-diskman_INCLUDE_kmod_md_linear=y
-CONFIG_PACKAGE_luci-app-diskman_INCLUDE_kmod_md_raid456=y
-CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
-CONFIG_PACKAGE_luci-app-diskman_INCLUDE_mdadm=y
-EOF
-
-# 其他插件设置选择:
-cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-adguardhome=y
-CONFIG_PACKAGE_luci-app-gpsysupgrade=y
-CONFIG_PACKAGE_luci-app-frpc=y
-CONFIG_PACKAGE_luci-app-jd-dailybonus=y
-CONFIG_PACKAGE_luci-app-mwan3=y
-CONFIG_PACKAGE_luci-app-mwan3helper=y
-CONFIG_PACKAGE_luci-app-nps=y
-CONFIG_PACKAGE_luci-app-poweroff=y
-CONFIG_PACKAGE_luci-app-serverchan=y
-#CONFIG_PACKAGE_luci-app-smartdns=y
-CONFIG_PACKAGE_luci-app-socat=y
-CONFIG_PACKAGE_luci-app-syncdial=y
-CONFIG_PACKAGE_luci-app-ttyd=y
-#CONFIG_PACKAGE_luci-app-eqos=y
-#CONFIG_PACKAGE_luci-app-wrtbwmon-zh=y
-#CONFIG_PACKAGE_smartdns=y
-EOF
-
-# 添加Passwall+:
-cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-bypass=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Shadowsocks_Server=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_ShadowsocksR_Server=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Simple_obfs=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Simple_obfs_server=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray_plugin=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Xray=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Trojan=y
-# CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Trojan-Go is not set
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_NaiveProxy=y
-# CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Kcptun is not set
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Socks5_Proxy=y
-CONFIG_PACKAGE_luci-app-bypass_INCLUDE_Socks_Server=y
-EOF
-
 
 # 
 # ========================固件定制部分结束========================
